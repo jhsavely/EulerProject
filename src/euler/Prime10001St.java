7a4ddf42d419e4c9a80ac89eea829f7e3f483bbd
John Savely <jh.savely@gmail.com>
2021-02-03 19:51:35 +0200

    solved: LargestProductInASerie fixed: minor style corrections

--- src/euler/Prime10001St.java
+++ src/euler/Prime10001St.java
@@ -1,8 +1,10 @@
 package euler;
 
 /**
- * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
- * that the 6th prime is 13. What is the 10 001st prime number?
+ * Problem 7.
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we
+ * can see that the 6th prime is 13.
+ * What is the 10001st prime number?
  *
  * @author jsavely
  */
@@ -15,21 +17,21 @@ public class Prime10001St {
 
     public void sieve() {
         primes.clear();
-        for (int N = 2; primes.size() < PRIME_INDEX; N++) {
+        for (int n = 2; primes.size() < PRIME_INDEX; n++) {//start from 2
             boolean isPrime = true;
             for (int j = 0; j < primes.size(); j++) {
-                long x = primes.get(j);
-                if (x * x > N) {
+                int prime = primes.get(j);
+                if (prime * prime > n) {
                     break;
                 }
-                if ((N % x) == 0) {
+                if ((n % prime) == 0) {
                     isPrime = false;
                     break;
                 }
             }
             if (isPrime) {
-                primes.add(N);
-                System.out.println(primes.size()+": "+N);
+                primes.add(n);
+                System.out.println(primes.size() + ": " + n);
             }
         }
     }
