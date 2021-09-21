c7af1df78212b9678066e6a709dfc8bfaf6b39af
John Savely <jh.savely@gmail.com>
2021-09-21 03:28:53 +0300

    polished: fibonacci numbers

--- src/euler/EvenFibonacciNumbers.java
+++ src/euler/EvenFibonacciNumbers.java
@@ -12,27 +12,15 @@ package euler;
  */
 public class EvenFibonacciNumbers {
 
-    public static long fibonacci(int n) {//recursive Fibonacci numbers, purpousely ineffective, for demonstration 
-        if (n <= 1) {
-            return n;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
-    }
-    
     public static void main(String[] args) {
-//        int n = Integer.parseInt(args[0]);
-//        System.out.println(fibonacci(n));
-//        for (int i = 1; i <= n; ++i) {
-//            System.out.println(i + ": " + fibonacci(i));
-//        }
+
         final int MAX_FIB = 4000000;
-        int iter_limit = 1000;//safety catch for while()
+        int iterationLimit = 1000;//safety catch for while()
         int fib0 = 0;
         int fib1 = 1;
         long s = 0;
 
-        while (fib1 < MAX_FIB && iter_limit > 0) {
+        while (fib1 < MAX_FIB && iterationLimit > 0) {
             if (fib1 % 2 == 0) {
                 s += fib1;
                 System.out.println("even: " + fib1 + " sum = " + s);
@@ -40,7 +28,7 @@ public class EvenFibonacciNumbers {
             int f = fib0 + fib1;
             fib0 = fib1;
             fib1 = f;
-            iter_limit--;
+            iterationLimit--;
         }
         System.out.println("sum of the even-valued terms: " + s);
     }
